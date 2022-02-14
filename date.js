7c0c616c75862af88fcc91638e98422601b01f18
Imrancu <mdimran3772@gmail.com>
2022-02-14 15:27:48 +0600

    Module_29 done

---
+++ date.js
@@ -0,0 +1,17 @@
+/* const myFavoriteDate = new Date('1971-12-16')
+    // console.log(myFavoriteDate);
+const anotherDate = new Date(1971, 3, 26, 11, 51, 5, 8);
+// console.log(anotherDate);
+
+if (myFavoriteDate.getTime() < anotherDate.getTime()) {
+    console.log('Favotite is earliar!')
+} */
+
+// for (let i == 0; i < 10; i++) {}
+
+const arraySort = ["121", "12", "1", "112", "111"];
+console.log(arraySort.sort());
+
+const task = "Practice Makes a person perfect";
+const index = task.toLowerCase().indexOf("M");
+console.log(index);
---
+++ try-catch.js
@@ -0,0 +1,18 @@
+let name = 'ami';
+const student = undefined;
+
+
+// console.log(student.name);
+
+try {
+    name = 'imran';
+} catch (error) {
+
+}
+console.log(name);
+
+// for (let i = 0; i < 10; i++) {
+
+// }
+// name = 'tumi';
+// console.log(name);
